76c8b341ad47a77d18e7a127e8a1347dfe4b6cc1
mayur123marathe <mayur123marathe@gmail.com>
2025-08-19 16:12:11 +0530

    added sass style for navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "../styles/css/style.css";
 
 import { useStore } from "store_remote/Store";
 function Navbar() {
@@ -9,67 +10,21 @@ function Navbar() {
     setShowCart(!showCart);
   };
   return (
-    <div
-      style={{
-        backgroundColor: "#935dff",
-        color: "#ffffffff",
-        padding: "10px",
-        paddingInline: "30px",
-        height: "50px",
-        justifyContent: "space-between",
-        alignItems: "center",
-        position: "relative",
-
-        display: "flex",
-      }}
-    >
+    <div className="nav">
       <div id="logo" className="header">
         Micro Frontend App
       </div>
       <div id="login"></div>
-      <span
-        className="cart"
-        onClick={clickHandler}
-        style={{
-          display: "inline-block",
-          width: "95%",
-          textAlign: "right",
-          fontSize: "20px",
-          padding: "5px",
-        }}
-      >
+      <span className="cart" onClick={clickHandler}>
         🛒 ({cart.length})
       </span>
       {showCart && (
-        <ul
-          className="cart-items "
-          style={{
-            width: "300px",
-            height: "200px",
-            border: "1px solid #ccc",
-            position: "absolute",
-            marginTop: "150px",
-            backgroundColor: "#fff",
-            right: "75px",
-            padding: "20px",
-            listStyleType: "none",
-            fontSize: "17px",
-            overflowY: "auto",
-            color: "black",
-            zIndex: 100,
-          }}
-        >
+        <ul className="cart-items ">
           {" "}
           Cart Items:
           {cart.map((cartItem) => {
             return (
-              <li
-                key={cartItem.id}
-                style={{
-                  padding: "10px",
-                  borderBottom: "1px dotted green",
-                }}
-              >
+              <li key={cartItem.id}>
                 {cartItem.name} ({cartItem.quantity})
               </li>
             );
